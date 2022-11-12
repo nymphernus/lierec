f1a719259d3492428dd5f008435f18a6dfef1860
Aleksey Schumann <103174654+aswatthaman@users.noreply.github.com>
2022-11-12 11:02:51 +0300

    Update pupil_tracker.py

--- pupil_tracker/pupil_tracker.py
+++ pupil_tracker/pupil_tracker.py
@@ -90,17 +90,3 @@ class pupilTracking(object):
         if self.pupils_located:
             blinking_ratio = (self.eye_left.blinking + self.eye_right.blinking) / 2
             return blinking_ratio > 3.8
-
-    def annotated_frame(self):
-        frame = self.frame.copy()
-
-        if self.pupils_located:
-            color = (0, 255, 0)
-            x_left, y_left = self.pupil_left_coords()
-            x_right, y_right = self.pupil_right_coords()
-            cv2.line(frame, (x_left - 5, y_left), (x_left + 5, y_left), color)
-            cv2.line(frame, (x_left, y_left - 5), (x_left, y_left + 5), color)
-            cv2.line(frame, (x_right - 5, y_right), (x_right + 5, y_right), color)
-            cv2.line(frame, (x_right, y_right - 5), (x_right, y_right + 5), color)
-
-        return frame
